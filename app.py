aa3ee9ad67cbcf7b25643b7cc43a6a7820e42389
Nikhitha <lingalanikhitha26@gmail.com>
2026-04-18 20:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
-
+import warnings
+warnings.filterwarnings("ignore")
 # =========================
 # 📦 Load Model (XGBoost)
 # =========================
